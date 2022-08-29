2eafe5de113febeefc2fa2911ef19315b5267d21
zsworker <zsworkermail@163.com>
2022-08-29 17:37:37 +0800

    提交文件

---
+++ tunnel-platform/src/main/java/com/tunnel/platform/service/event/impl/SdTunnelSubareaServiceImpl.java
@@ -0,0 +1,97 @@
+package com.tunnel.platform.service.event.impl;
+
+import java.util.List;
+import com.ruoyi.common.utils.DateUtils;
+import com.tunnel.platform.domain.event.SdTunnelSubarea;
+import com.tunnel.platform.mapper.event.SdTunnelSubareaMapper;
+import com.tunnel.platform.service.event.ISdTunnelSubareaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+/**
+ * 隧道分区Service业务层处理
+ *
+ * @author ruoyi
+ * @date 2022-08-25
+ */
+@Service
+public class SdTunnelSubareaServiceImpl implements ISdTunnelSubareaService
+{
+    @Autowired
+    private SdTunnelSubareaMapper sdTunnelSubareaMapper;
+
+    /**
+     * 查询隧道分区
+     *
+     * @param sId 隧道分区主键
+     * @return 隧道分区
+     */
+    @Override
+    public SdTunnelSubarea selectSdTunnelSubareaBySId(Long sId)
+    {
+        return sdTunnelSubareaMapper.selectSdTunnelSubareaBySId(sId);
+    }
+
+    /**
+     * 查询隧道分区列表
+     *
+     * @param sdTunnelSubarea 隧道分区
+     * @return 隧道分区
+     */
+    @Override
+    public List<SdTunnelSubarea> selectSdTunnelSubareaList(SdTunnelSubarea sdTunnelSubarea)
+    {
+        return sdTunnelSubareaMapper.selectSdTunnelSubareaList(sdTunnelSubarea);
+    }
+
+    /**
+     * 新增隧道分区
+     *
+     * @param sdTunnelSubarea 隧道分区
+     * @return 结果
+     */
+    @Override
+    public int insertSdTunnelSubarea(SdTunnelSubarea sdTunnelSubarea)
+    {
+        sdTunnelSubarea.setCreateTime(DateUtils.getNowDate());
+        return sdTunnelSubareaMapper.insertSdTunnelSubarea(sdTunnelSubarea);
+    }
+
+    /**
+     * 修改隧道分区
+     *
+     * @param sdTunnelSubarea 隧道分区
+     * @return 结果
+     */
+    @Override
+    public int updateSdTunnelSubarea(SdTunnelSubarea sdTunnelSubarea)
+    {
+        sdTunnelSubarea.setUpdateTime(DateUtils.getNowDate());
+        return sdTunnelSubareaMapper.updateSdTunnelSubarea(sdTunnelSubarea);
+    }
+
+    /**
+     * 批量删除隧道分区
+     *
+     * @param sIds 需要删除的隧道分区主键
+     * @return 结果
+     */
+    @Override
+    public int deleteSdTunnelSubareaBySIds(Long[] sIds)
+    {
+        return sdTunnelSubareaMapper.deleteSdTunnelSubareaBySIds(sIds);
+    }
+
+    /**
+     * 删除隧道分区信息
+     *
+     * @param sId 隧道分区主键
+     * @return 结果
+     */
+    @Override
+    public int deleteSdTunnelSubareaBySId(Long sId)
+    {
+        return sdTunnelSubareaMapper.deleteSdTunnelSubareaBySId(sId);
+    }
+}

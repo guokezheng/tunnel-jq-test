c9e5ec00d5322df68733670bbabd5575591308a7
ChinaLinked <chinalink2022@163.com>
2022-08-30 09:04:06 +0800

    提交隧道分区

---
+++ tunnel-platform/src/main/java/com/tunnel/platform/domain/event/SdTunnelSubarea.java
@@ -0,0 +1,76 @@
+package com.tunnel.platform.domain.event;
+
+import com.tunnel.platform.domain.dataInfo.SdTunnels;
+import lombok.Data;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+import com.ruoyi.common.annotation.Excel;
+import com.ruoyi.common.core.domain.BaseEntity;
+
+import java.util.List;
+
+/**
+ * 隧道分区对象 sd_tunnel_subarea
+ *
+ * @author ruoyi
+ * @date 2022-08-25
+ */
+@Data
+public class SdTunnelSubarea extends BaseEntity
+{
+    private static final long serialVersionUID = 1L;
+
+    /** 分区id */
+    private Long sId;
+
+    /** 分区名称 */
+    @Excel(name = "分区名称")
+    private String sName;
+
+    /** 隧道id */
+    @Excel(name = "隧道id")
+    private String tunnelId;
+
+    private String tunnelName;
+
+    public void setsId(Long sId)
+    {
+        this.sId = sId;
+    }
+
+    public Long getsId()
+    {
+        return sId;
+    }
+    public void setsName(String sName)
+    {
+        this.sName = sName;
+    }
+
+    public String getsName()
+    {
+        return sName;
+    }
+    public void setTunnelId(String tunnelId)
+    {
+        this.tunnelId = tunnelId;
+    }
+
+    public String getTunnelId()
+    {
+        return tunnelId;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this,ToStringStyle.MULTI_LINE_STYLE)
+            .append("sId", getsId())
+            .append("sName", getsName())
+            .append("tunnelId", getTunnelId())
+            .append("createBy", getCreateBy())
+            .append("createTime", getCreateTime())
+            .append("updateBy", getUpdateBy())
+            .append("updateTime", getUpdateTime())
+            .toString();
+    }
+}

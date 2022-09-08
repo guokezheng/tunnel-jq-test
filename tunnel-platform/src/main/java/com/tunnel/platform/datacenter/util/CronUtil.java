620724ef5ad1a16fd6e9e998eb06617c3c8ec2bc
xyw <xw887517@163.com>
2022-09-08 17:05:38 +0800

    cron转换工具类

--- tunnel-platform/src/main/java/com/tunnel/platform/datacenter/util/CronUtil.java
+++ tunnel-platform/src/main/java/com/tunnel/platform/datacenter/util/CronUtil.java
@@ -4,7 +4,14 @@ package com.tunnel.platform.datacenter.util;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
+
 public class CronUtil {
+    /**
+     * 时分秒转换Cron表达式
+     * @param time
+     * @return
+     */
     public static String CronDate(String time){
 
         SimpleDateFormat dataformatter = new SimpleDateFormat("HH:mm:ss");

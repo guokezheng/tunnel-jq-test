a2d850c4da7061d18ac179743bef0833fdb11c75
“wu” <1921085111@qq.com>
2022-09-20 17:22:44 +0800

    SQL查询接口注解依赖包修正代码提交 --- 吴昊阳

--- tunnel-business/src/main/java/com/tunnel/business/mapper/dataInfo/SdDeviceDataMapper.java
+++ tunnel-business/src/main/java/com/tunnel/business/mapper/dataInfo/SdDeviceDataMapper.java
@@ -1,7 +1,7 @@
 package com.tunnel.business.mapper.dataInfo;
 
 import com.tunnel.business.domain.dataInfo.SdDeviceData;
-import io.lettuce.core.dynamic.annotation.Param;
+import org.apache.ibatis.annotations.Param;;
 
 import java.util.List;
 import java.util.Map;

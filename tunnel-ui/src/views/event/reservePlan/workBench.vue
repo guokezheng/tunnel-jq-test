27a3ad1a7f2192a3824388fff50486ad4a7abbd1
Praise-Sun <18053314396@163.com>
2022-11-11 09:38:38 +0800

    预览弹窗修改
    样式修改

--- tunnel-ui/src/views/event/reservePlan/workBench.vue
+++ tunnel-ui/src/views/event/reservePlan/workBench.vue
@@ -2,7 +2,7 @@
  * @Author: Praise-Sun 18053314396@163.com
  * @Date: 2022-10-17 14:42:00
  * @LastEditors: Praise-Sun 18053314396@163.com
- * @LastEditTime: 2022-11-10 09:15:47
+ * @LastEditTime: 2022-11-11 09:37:14
  * @FilePath: \tunnel-ui\src\views\event\reservePlan\workBench.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -13,7 +13,7 @@
       :visible.sync="workbenchOpen"
       append-to-body
       title="预览"
-      width="1660px"
+      width="1620px"
       class="workbenchBox"
     >
       <img alt="" class="chedaoImage" :src="currentTunnel" />
@@ -426,8 +426,10 @@ export default {
     }
   }
 }
-
 </style>
 <style>
-  .workbenchBox .el-dialog__body{position: relative;padding: 0px!important;}
+.workbenchBox .el-dialog__body {
+  position: relative;
+  padding: 0px !important;
+}
 </style>

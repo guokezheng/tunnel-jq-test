b6ef7f4ab85e4108e691ef3bcda189460b432e1f
jwei123456 <1070521297@qq.com>
2022-11-16 13:11:15 +0800

    数据库表维护

--- sql/表结构以及字典值修改.sql
+++ sql/表结构以及字典值修改.sql
@@ -372,11 +372,11 @@ INSERT INTO sys_menu(menu_id, menu_name, parent_id, order_num, path, component, 
 -- 修改sd_traffic_image表字段img_url的字段类型
 alter table sd_traffic_image modify column img_url longtext comment '图片url';
 
--- 修改sd_task_liat表字段task_cxtime的字段类型
-alter table sd_task_liat column task_cxtime varchar(25) comment '任务持续时间';
+-- 修改sd_task_list表字段task_cxtime的字段类型
+alter table sd_task_list column task_cxtime varchar(25) comment '任务持续时间';
 
--- 修改sd_fault_liat表字段fault_cxtime的字段类型
-alter table sd_fault_liat column fault_cxtime varchar(25) comment '故障持续时间（根据当前时间与故障发现时间计算时间差，单位：天、小时；计算时间差，不保存只展示；记录变为“已消除”状态时，保存持续时间）';
+-- 修改sd_fault_list表字段fault_cxtime的字段类型
+alter table sd_fault_list column fault_cxtime varchar(25) comment '故障持续时间（根据当前时间与故障发现时间计算时间差，单位：天、小时；计算时间差，不保存只展示；记录变为“已消除”状态时，保存持续时间）';
 
 -- 数据字典新增设备模拟控制开关标识
 INSERT INTO `sys_dict_type`(`dict_name`, `dict_type`, `status`, `create_by`, `create_time`, `update_by`, `update_time`, `remark`) VALUES ('设备控制模拟值', 'sys_analog_control_isopen', '0', 'admin', '2022-11-14 11:01:31', '', NULL, NULL);

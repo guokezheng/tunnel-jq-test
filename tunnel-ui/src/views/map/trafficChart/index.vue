7700f67f9acd1f35adc1d5a261de2bc486c11c20
ganyimei <472374726@qq.com>
2022-12-01 17:09:44 +0800

    数字孪生

---
+++ tunnel-ui/src/views/map/trafficChart/index.vue
@@ -0,0 +1,41 @@
+<template>
+    <!-- 交通监测 -->
+    <div style="height:38">
+      <iframe
+        name="tuniframe"
+        id="miframe"
+        class="mapHuaWei"
+        frameborder="0"
+        align="center"
+        allowfullscreen="true"
+        allow="autoplay"
+        src="http://106.120.201.126:14712/trafficChart"
+      ></iframe>
+    </div>
+  </template>
+  <script>
+  import $ from "jquery";
+  export default {
+    data() {
+      return {};
+    },
+    created() {},
+  };
+  </script>
+  <style scoped>
+  .mapHuaWei {
+    height: calc(100% + 10px);
+    position: relative;
+    top: -10px;
+    z-index: 3;
+    width: 100%;
+  }
+  /* .mapBox {
+    position: fixed;
+    top: 0px;
+    left: 0px;
+    z-index: 960619;
+    width: 100%;
+  } */
+  </style>
+  

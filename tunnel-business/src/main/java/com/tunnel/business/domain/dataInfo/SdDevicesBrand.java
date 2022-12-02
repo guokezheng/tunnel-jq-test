bd305c20464ce4c7dcb2193478cca39e3e8943a2
ChinaLinked <chinalink2022@163.com>
2022-12-02 19:48:54 +0800

    同步集团部门数据 定时任务接口和对外接口

--- tunnel-business/src/main/java/com/tunnel/business/domain/dataInfo/SdDevicesBrand.java
+++ tunnel-business/src/main/java/com/tunnel/business/domain/dataInfo/SdDevicesBrand.java
@@ -1,53 +1,81 @@
 package com.tunnel.business.domain.dataInfo;
 
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+import com.ruoyi.common.annotation.Excel;
+import com.ruoyi.common.core.domain.BaseEntity;
 
 /**
- * 设备品牌
+ * 物联设备厂商对象 sd_devices_brand
+ *
+ * @author ruoyi
+ * @date 2022-12-02
  */
-@ApiModel("设备品牌对象类")
-public class SdDevicesBrand {
-    @ApiModelProperty("设备厂商ID")
+public class SdDevicesBrand extends BaseEntity {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 设备厂商编号
+     */
     private String supplierId;
 
-    @ApiModelProperty("设备厂商名称")
+    /**
+     * 设备厂商名称
+     */
+    @Excel(name = "设备厂商名称")
     private String supplierName;
 
-    @ApiModelProperty("简称")
+    /**
+     * 简称
+     */
+    @Excel(name = "简称")
     private String shortName;
 
-    public String getSupplierId() {
-        return supplierId;
-    }
+    private Integer isDel;
 
     public void setSupplierId(String supplierId) {
         this.supplierId = supplierId;
     }
 
-    public String getSupplierName() {
-        return supplierName;
+    public String getSupplierId() {
+        return supplierId;
     }
 
     public void setSupplierName(String supplierName) {
         this.supplierName = supplierName;
     }
 
-    public String getShortName() {
-        return shortName;
+    public String getSupplierName() {
+        return supplierName;
     }
 
     public void setShortName(String shortName) {
         this.shortName = shortName;
     }
 
+    public String getShortName() {
+        return shortName;
+    }
+
+    public Integer getIsDel() {
+        return isDel;
+    }
+
+    public void setIsDel(Integer isDel) {
+        this.isDel = isDel;
+    }
 
     @Override
     public String toString() {
-        return "SdDevicesBrand{" +
-                "supplierId='" + supplierId + '\'' +
-                ", supplierName='" + supplierName + '\'' +
-                ", shortName='" + shortName + '\'' +
-                '}';
+        return new ToStringBuilder(this, ToStringStyle.MULTI_LINE_STYLE)
+                .append("supplierId", getSupplierId())
+                .append("supplierName", getSupplierName())
+                .append("shortName", getShortName())
+                .append("createBy", getCreateBy())
+                .append("createTime", getCreateTime())
+                .append("updateBy", getUpdateBy())
+                .append("updateTime", getUpdateTime())
+                .append("remark", getRemark())
+                .toString();
     }
 }

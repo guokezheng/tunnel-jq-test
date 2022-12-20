1575760c62bd5d5189959016299af8638e9a6d60
Mr-Macro <1341766395@qq.com>
2022-12-20 16:21:45 +0800

    主动安全-预警处置下发接口新增事件处置状态更新

--- tunnel-platform/src/main/java/com/tunnel/platform/service/event/impl/SdStrategyServiceImpl.java
+++ tunnel-platform/src/main/java/com/tunnel/platform/service/event/impl/SdStrategyServiceImpl.java
@@ -1061,6 +1061,8 @@ public class SdStrategyServiceImpl implements ISdStrategyService {
         int issueResult = 0;
         for(SdStrategyRl rl:rlList){
             issueResult = issuedDevice(rl,eventId,"0");
+            //更新事件处置记录表状态
+            updateHandleState(rl.getId(),eventId);
         }
         return issueResult;
     }
@@ -1068,7 +1070,10 @@ public class SdStrategyServiceImpl implements ISdStrategyService {
     @Override
     public int implementDisposalStrategyRl(Long rlId,Long eventId) {
         SdStrategyRl rl = sdStrategyRlMapper.selectSdStrategyRlById(rlId);
-        return issuedDevice(rl,eventId,"0");
+        int count = issuedDevice(rl, eventId, "0");
+        //更新事件处置记录表状态
+        updateHandleState(rl.getId(),eventId);
+        return count;
     }
 
     /**

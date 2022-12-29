6579b9076b30cd3890a6955aef53b18bcc12c68c
jwei123456 <1070521297@qq.com>
2022-12-29 14:33:08 +0800

    环境监测视频播放

--- tunnel-ui/src/views/monitor/operatingEnvironment/index.vue
+++ tunnel-ui/src/views/monitor/operatingEnvironment/index.vue
@@ -214,7 +214,7 @@ import LoopTree from '@/views/components/powerTree/eTree.vue'
 import { powerTreeselect } from '@/api/configcenter/power'
 import { getTypeListApi, getTypeListXqApi, getSelectListEqApi, getSelectListDataEqApi, fixIndustryModelApi, getCameraListApi } from '@/api/panoramicData/index.js'
 import {getToken} from "@/api/panoramicData/indexToken";
-/*import video from '@/components/videoPlayer/myVideo.vue'*/
+import videos from '../../components/videoPlayer/myVideo.vue'
 export default {
   name: 'operatingEnvironment',
   data() {
@@ -296,7 +296,7 @@ export default {
 
   props: {},
   // 注册组件
-  components: { echartsEnvironment, LoopTree, echartsIndustry/*, video*/ },
+  components: { echartsEnvironment, LoopTree, echartsIndustry, videos },
   // 计算属性
   computed: {},
   // 生周期函数

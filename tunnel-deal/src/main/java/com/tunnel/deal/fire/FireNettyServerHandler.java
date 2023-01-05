261d70c654037a54aab2d6521300c1081a5005a1
zsworker <zsworkermail@163.com>
2023-01-05 16:07:53 +0800

    火灾报警代码修改

--- tunnel-deal/src/main/java/com/tunnel/deal/fire/FireNettyServerHandler.java
+++ tunnel-deal/src/main/java/com/tunnel/deal/fire/FireNettyServerHandler.java
@@ -90,7 +90,7 @@ public class FireNettyServerHandler extends ChannelInboundHandlerAdapter {
                 return;
             }
 
-            if (data.contains("恢复")) {
+            if (data.contains("恢复") || data.contains("全部声光停止")) {
                 log.info("主机{}进行了复位，事件类型为{}", system.getSystemUrl(), alarmType);
                 //复位清除设备报警状态
                 SdDevices dev = new SdDevices();

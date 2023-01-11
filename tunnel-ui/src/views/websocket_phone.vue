ca99b29d8e791ebf75bbbb3343dec002212cd35d
zsworker <zsworkermail@163.com>
2023-01-11 14:32:23 +0800

    广播电话websocket修改

--- tunnel-ui/src/views/websocket_phone.vue
+++ tunnel-ui/src/views/websocket_phone.vue
@@ -10,39 +10,45 @@
   export default {
     name: "Websocket",
     data() {
-      return {};
+      return {
+        websocket: null
+      };
     },
-    computed: {
-      ...mapState({
-        token: (state) => state.user.token,
-        websocket: (state) => state.user.websocket,
-      }),
-    },
-    watch: {
-      websocket({password, path, port, interval}) {
-        // 建立 websocket 连接
-        this.socket.initialize({
-          // url: 'ws://' + location.hostname + ':' + port + path,
-          url: 'ws://10.7.187.6:8981',
-          password: password,
-          tokenSN: this.token,
-          heartRate: interval,
-        });
-        this.socket.onopen = () => {
-        };
-        this.socket.onmessage = (message) => {
-          // console.log("电话广播websocket>>>>>>>>>>>>",message)
-          // console.log("电话广播websocket>>>>>>>>>>>>",this.isJson(message))
-          if (this.isJson(message)) {
-            onMessage(message).then(response => {
-              console.log(response)
-            });
-          }
-        };
-      },
-    },
-    created() {
-    },
+    // computed: {
+    //   ...mapState({
+    //     token: (state) => state.user.token,
+    //     websocket: (state) => state.user.websocket,
+    //   }),
+    // },
+    // watch: {
+    //   websocket({password, path, port, interval}) {
+    //     建立 websocket 连接
+    //     this.socket.initialize({
+    //       // url: 'ws://' + location.hostname + ':' + port + path,
+    //       url: 'ws://10.7.187.6:8981',
+    //       password: password,
+    //       tokenSN: this.token,
+    //       heartRate: interval,
+    //     });
+    //     this.socket.onopen = () => {
+    //     };
+    //     this.socket.onmessage = (message) => {
+    //       // console.log("电话广播websocket>>>>>>>>>>>>",message)
+    //       // console.log("电话广播websocket>>>>>>>>>>>>",this.isJson(message))
+    //       if (this.isJson(message)) {
+    //         onMessage(message).then(response => {
+    //           console.log(response)
+    //         });
+    //       }
+    //     };
+    //   },
+    // },
+  created() {
+    this.initWebSocket();
+  },
+  destroyed() {
+    this.websocket.close() //离开路由之后断开websocket连接
+  },
     methods: {
       isJson(str){
         if (typeof str == 'string') {
@@ -58,6 +64,33 @@
             return false;
           }
         }
+      },
+      initWebSocket(){ //初始化weosocket
+        // var url = "ws://localhost:8000/websocket";
+        var url = "ws://10.7.187.6:8981";
+        this.websocket = new WebSocket(url);
+        this.websocket.onmessage = this.websocketonmessage;
+        this.websocket.onopen = this.websocketonopen;
+        this.websocket.onerror = this.websocketonerror;
+        this.websocket.onclose = this.websocketclose;
+      },
+      websocketonopen(){
+        console.log("紧急电话广播系统建立连接");
+      },
+      websocketonerror(){//连接建立失败重连
+        console.log("紧急电话广播系统连接建立失败重连");
+        setTimeout(5000,this.initWebSocket());
+        // this.initWebSocket();
+      },
+      websocketonmessage(message){ //数据接收
+        if (this.isJson(message)) {
+          onMessage(message).then(response => {
+            console.log(response)
+        });
+        }
+      },
+      websocketclose(e){  //关闭
+        console.log('紧急电话广播系统断开连接',e);
       },
     },
   };

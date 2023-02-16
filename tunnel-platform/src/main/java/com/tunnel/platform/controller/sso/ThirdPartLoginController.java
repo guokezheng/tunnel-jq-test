c4da5632668e948d2f00b824221476531d731c4b
ChinaLinked <chinalink2022@163.com>
2023-02-16 09:47:40 +0800

    无效代码

--- tunnel-platform/src/main/java/com/tunnel/platform/controller/sso/ThirdPartLoginController.java
+++ tunnel-platform/src/main/java/com/tunnel/platform/controller/sso/ThirdPartLoginController.java
@@ -1,16 +1,12 @@
 package com.tunnel.platform.controller.sso;
 
 import com.ruoyi.common.core.domain.AjaxResult;
-import com.ruoyi.common.core.domain.entity.SysDept;
-import com.ruoyi.system.mapper.SysDeptMapper;
 import com.tunnel.platform.service.sso.ThirdPartLoginService;
-import com.tunnel.platform.util.AuthUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -25,9 +21,6 @@ public class ThirdPartLoginController {
 
     @Autowired
     private ThirdPartLoginService thirdPartLoginService;
-
-    @Autowired
-    private AuthUtil authUtil;
 
 
     /**
@@ -55,48 +48,6 @@ public class ThirdPartLoginController {
 
         String code = Optional.ofNullable(loginInfo.get("code")).orElse("").toString();
         return thirdPartLoginService.login(code);
-    }
-
-    @Autowired
-    private SysDeptMapper deptMapper;
-
-    @PostMapping(value = "/getTreeAll")
-    public AjaxResult getDeptTree(String id) {
-        // List<Map<String, Object>> deptTree = thirdPartLoginService.getDeptTree("5b7a963a-6363-4ca9-b691-b534835c4bcb", id);
-        // thirdPartLoginService.metho();
-
-        // 获取授权码
-        String authCode = AuthUtil.getAuthCode("admin");
-        // 获取token
-        String token = AuthUtil.getToken(authCode);
-        // 获取部门树
-        List<Map<String, Object>> deptTree = AuthUtil.getDeptTree(token, null);
-        // 部门树转List
-        List<Map<String, Object>> deptList = new ArrayList<>();
-        AuthUtil.treeToList(deptTree, deptList);
-
-        if (deptList.size() > 0) {
-            SysDept sysDept = new SysDept();
-            for (Map<String, Object> map : deptList) {
-                String jtDeptId = (String) map.get("id");
-                String pid = (String) map.get("pid");
-                String name = (String) map.get("name");
-                String sort = (String) map.get("sort");
-
-
-                sysDept.setDeptName(name);
-                sysDept.setOrderNum(sort);
-
-//                SysDept dept = deptMapper.selectJtDeptById(jtDeptId);
-//                if (null!= dept) {
-//                    deptMapper.updateDept(sysDept);
-//                }else {
-//                    deptMapper.insertDept(sysDept);
-//                }
-            }
-        }
-
-        return AjaxResult.success();
     }
 
     @PostMapping(value = "/getUserListByCompanyAndDept")

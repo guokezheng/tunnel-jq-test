4e2eb805782103e810883944ebf6919eb21354e7
guozheng <274587520@qq.com>
2023-04-04 17:56:52 +0800

    【修正】--郭正

--- tunnel-ui/src/views/monitor/logdatainfo/index.vue
+++ tunnel-ui/src/views/monitor/logdatainfo/index.vue
@@ -380,7 +380,7 @@
         prop="stateName.stateName"
         :formatter="operationStateFormat"
       />
-      <el-table-column label="控制方式" align="center" prop="controlType" />
+      <el-table-column label="控制方式" align="center" prop="controlType" :formatter="controlTypeFormat" />
       <el-table-column label="操作结果" align="center" prop="state" />
       <el-table-column label="操作地址" align="center" prop="operIp" />
       <el-table-column

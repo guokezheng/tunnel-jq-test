43ae14ba094f0190dfc0fa96aff82ee9f34e2dc1
Praise-Sun <18053314396@163.com>
2023-04-10 10:48:28 +0800

    应急预案问题修改

--- tunnel-ui/src/views/event/reservePlan/index.vue
+++ tunnel-ui/src/views/event/reservePlan/index.vue
@@ -2,7 +2,7 @@
  * @Author: Praise-Sun 18053314396@163.com
  * @Date: 2022-12-08 15:17:28
  * @LastEditors: Praise-Sun 18053314396@163.com
- * @LastEditTime: 2023-04-08 11:37:34
+ * @LastEditTime: 2023-04-10 10:35:33
  * @FilePath: \tunnel-ui\src\views\event\reservePlan\index.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -1164,6 +1164,7 @@ export default {
         });
     },
     addInfo(index) {
+      
       let data = {
         processStageName: "",
         processesList: [
@@ -1174,6 +1175,7 @@ export default {
             state: "",
             qbbState: "",
             retrievalRule: null, //规则条件
+            retrievalRuleList:[],
             equipmentTypeData: [],
             equipmentData: [],
             eqStateList: [],
@@ -1182,6 +1184,7 @@ export default {
           },
         ],
       };
+      this.getRules();
       this.planTypeIdList.splice(index + 1, 0, data);
     },
     /** 查询设备类型列表 */

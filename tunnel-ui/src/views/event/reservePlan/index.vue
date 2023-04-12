9792ed338c2b099bc68a8a687de9b130fc5fae77
Praise-Sun <18053314396@163.com>
2023-04-12 08:51:40 +0800

    时间修改

--- tunnel-ui/src/views/event/reservePlan/index.vue
+++ tunnel-ui/src/views/event/reservePlan/index.vue
@@ -2,7 +2,7 @@
  * @Author: Praise-Sun 18053314396@163.com
  * @Date: 2022-12-08 15:17:28
  * @LastEditors: Praise-Sun 18053314396@163.com
- * @LastEditTime: 2023-04-11 17:25:14
+ * @LastEditTime: 2023-04-12 08:51:13
  * @FilePath: \tunnel-ui\src\views\event\reservePlan\index.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -1105,7 +1105,7 @@ export default {
       const Loading = this.$loading(this.maskOptions);
       setTimeout(()=>{
         Loading.close();
-      },1200)
+      },1000)
     },
     getRules() {
       this.getDicts("sd_device_retrieval_rule").then((response) => {

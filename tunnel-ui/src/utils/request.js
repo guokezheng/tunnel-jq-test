a05053eb6f2ae82ae3798eb57aea527266c799a0
zsworker <zsworkermail@163.com>
2023-07-18 15:51:00 +0800

    重新登录跳转到空白页问题修改：windows nginx配置通过测试

--- tunnel-ui/src/utils/request.js
+++ tunnel-ui/src/utils/request.js
@@ -67,7 +67,7 @@ service.interceptors.response.use(res => {
         }
       ).then(() => {
         store.dispatch('LogOut').then(() => {
-          location.href = '/index'
+          location.href = '/'
         })
       }).catch(() => {});
       return Promise.reject('无效的会话，或者会话已过期，请重新登录。')

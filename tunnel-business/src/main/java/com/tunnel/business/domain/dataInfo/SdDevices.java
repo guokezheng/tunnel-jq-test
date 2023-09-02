69654e2419d1faaa5452367fd06f26b5e0df46f1
Mr-Macro <1341766395@qq.com>
2023-09-02 09:12:16 +0800

    设备管理导出 - 翟昌宏

--- tunnel-business/src/main/java/com/tunnel/business/domain/dataInfo/SdDevices.java
+++ tunnel-business/src/main/java/com/tunnel/business/domain/dataInfo/SdDevices.java
@@ -335,8 +335,8 @@ public class SdDevices<SdEquipmentStateIconFile> extends BaseEntity {
     /**
      * 设备状态	1-故障，2-告警
      */
-    @Excel(name = "设备状态 1-在线，2-离线，3-故障，4-报警",dictType = "sd_monitor_state",type= Excel.Type.ALL)
-    @ApiModelProperty("设备状态\t1-在线，2-离线，3-故障，4-报警")
+    @Excel(name = "设备状态",dictType = "sd_monitor_state",type= Excel.Type.ALL)
+    @ApiModelProperty("设备状态")
     private String eqStatus;
 
     /**

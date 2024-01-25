3204aafa8c52cf63928ecb52f36a99864529c1ca
Praise-Sun <18053314396@163.com>
2024-01-25 15:41:21 +0800

    页面bug修改

--- tunnel-ui/src/views/event/event/dispatch.vue
+++ tunnel-ui/src/views/event/event/dispatch.vue
@@ -2,7 +2,7 @@
  * @Author: Praise-Sun 18053314396@163.com
  * @Date: 2023-02-14 14:26:29
  * @LastEditors: Praise-Sun 18053314396@163.com
- * @LastEditTime: 2023-05-16 15:02:51
+ * @LastEditTime: 2024-01-25 15:38:13
  * @FilePath: \tunnel-ui\src\views\event\event\dispatch.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -20,12 +20,12 @@
         src="http://10.7.179.141:9528/tunnel_hushan"
       ></iframe>
     </div>
-    <div class="workBenchButton">
+    <!-- <div class="workBenchButton">
       <img
         src="../../../assets/cloudControl/tunnelBox2.png"
         @click="openWorkBench()"
       />
-    </div>
+    </div> -->
     <div class="drawerBox" @click="drawerHandleOpen()" >
       <i class="el-icon-d-arrow-left" v-show="drawer"></i>
       <i class="el-icon-d-arrow-right" v-show="drawer == false"></i>
